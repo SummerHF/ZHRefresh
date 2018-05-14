dc029608ff4851c8522d8074e0b92e2a60cdd02d
SummerHF <391565521@qq.com>
2018-05-14 17:01:24 +0800

    uicollectionView finished

--- Example/ZHRefresh/DemoViewController/CollectionViewController.swift
+++ Example/ZHRefresh/DemoViewController/CollectionViewController.swift
@@ -25,11 +25,27 @@
 //  THE SOFTWARE.
 //
 
-import UIKit
+import ZHRefresh
 
 private let reuseIdentifier = "Cell"
 
 class CollectionViewController: UICollectionViewController {
+
+    /// 颜色数组
+    private var colorArray: [UIColor] = [UIColor]()
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 80)
+        layout.minimumLineSpacing = 20
+        layout.minimumInteritemSpacing = 20
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        super.init(collectionViewLayout: layout)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,45 +54,57 @@ class CollectionViewController: UICollectionViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Register cell classes
+        self.collectionView?.backgroundColor = UIColor.white
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-
+        self.perform(Selector(self.method))
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    // MARK: - UICollectionView 上下拉刷新
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    @objc private func action18() {
+        self.collectionView?.header = ZHRefreshNormalHeader.headerWithRefreshing { [weak self] in
+            guard let `self` = self else { return }
+            for _ in 0..<5 {
+                self.colorArray.insert(self.randomColor(), at: 0)
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.collectionView?.reloadData()
+                self.collectionView?.header?.endRefreshing()
+            })
+        }
+        self.collectionView?.header?.beginRefreshing()
+        self.collectionView?.footer = ZHRefreshBackNormalFooter.footerWithRefreshing { [weak self] in
+            guard let `self` = self else { return }
+            for _ in 0..<5 {
+                self.colorArray.append(self.randomColor())
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0, execute: {
+                self.collectionView?.reloadData()
+                self.collectionView?.footer?.endRefreshing()
+            })
+        }
+        /// 默认先隐藏
+        self.collectionView?.footer?.isHidden = true
     }
-    */
 
     // MARK: UICollectionViewDataSource
 
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        /// 设置尾部刷新控件的隐藏
+        self.collectionView?.footer?.isHidden = self.colorArray.count == 0
+        return self.colorArray.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
-    
+        cell.backgroundColor = self.colorArray[indexPath.row]
         return cell
+    }
+
+    private func randomColor() -> UIColor {
+        return UIColor(red: CGFloat(arc4random_uniform(255)) / 255.0, green: CGFloat(arc4random_uniform(255)) / 255.0, blue: CGFloat(arc4random_uniform(255)) / 255.0, alpha: 1.0)
     }
 
     // MARK: UICollectionViewDelegate
